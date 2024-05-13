d5a83152d57ae453fa29bc39c08c30fa3b444c6b
Matt Barker <mattbarker23@hotmail.com>
2024-05-13 15:21:16 +0100

    First commit

---
+++ fakeSite/src/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <>
+      <div className="header-container">
+        <div className="logo">LOGO</div>
+        <div className="header-elements-container">
+          <div className="header-element">HEADER</div>
+          <div className="header-element">HEADER</div>
+          <div className="header-element">HEADER</div>
+          <div className="header-element">HEADER</div>
+          <div className="header-element">HEADER</div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ fakeSite/src/Home.jsx
@@ -0,0 +1,23 @@
+export default function Home() {
+  return (
+    <>
+      <div className="all-home-boxes">
+        <div className="top-boxes">
+          <div className="home-box">1</div>
+          <div className="home-box">2</div>
+          <div className="home-box">3</div>
+        </div>
+        <div className="mid-boxes">
+          <div className="home-box">4</div>
+          <div className="home-box">5</div>
+          <div className="home-box">6</div>
+        </div>
+        <div className="bottom-boxes">
+          <div className="home-box">7</div>
+          <div className="home-box">8</div>
+          <div className="home-box">9</div>
+        </div>
+      </div>
+    </>
+  );
+}
